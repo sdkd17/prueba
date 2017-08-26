b54c1e38b1e74cd8092244c2ab422ea3b5ddf690
coso123a <sergiopilu@hotmail.com>
2017-08-26 10:38:24 -0300

    desde linea de comandos

--- src/logica/Fabrica.java
+++ src/logica/Fabrica.java
@@ -19,6 +19,7 @@ public class Fabrica {
         }
         return instancia;
     }
+    //.........................................................
 
     public IControladorUsuario getIControladorUsuario() {
         return new ControladorUsuario();
